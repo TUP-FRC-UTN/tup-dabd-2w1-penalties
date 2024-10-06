78f931dbc4b55ad9b02a7e2157937750a60be05b
405742-VegaAngel-1w2 <405742@tecnicatura.frc.utn.edu.ar>
2024-10-06 20:41:56 -0300

    creacion de carpetas de componentes
    Co-Authored-By: 110899-SarateLuciana <63590410+110899-SarateLuciana-2w1@users.noreply.github.com>
    Co-Authored-By: 113857DeMaussionGabriel <164547690+113857DeMaussionGabriel@users.noreply.github.com>
    Co-Authored-By: 113914-MolinaTorres-Julian <128857318+113914-MolinaTorres-Julian@users.noreply.github.com>
    Co-Authored-By: 114264 - Moszoro Mateo <142827268+114264MoszoroMateo@users.noreply.github.com>
    Co-Authored-By: Lautaro Ariel Devegili <142621609+404942-Lautaro-Devegili@users.noreply.github.com>
    Co-Authored-By: 405708_VazquezFlexes_Martin <164551161+405708@users.noreply.github.com>

---
+++ src/app/components/consultar-denuncia-modal/consultar-denuncia-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-denuncia-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './consultar-denuncia-modal.component.html',
+  styleUrl: './consultar-denuncia-modal.component.scss'
+})
+export class ConsultarDenunciaModalComponent {
+
+}
