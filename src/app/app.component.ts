b2e1d5c754b0e18299022f24bd6baf41d3686f7b
405742-VegaAngel-1w2 <405742@tecnicatura.frc.utn.edu.ar>
2024-10-07 22:12:35 -0300

    consultar Informe Modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConsultarDenunciaModalComponent } from './components/consultar-denuncia-modal/consultar-denuncia-modal.component';
+import { ConsultarInformeModalComponent } from "./components/consultar-informe-modal/consultar-informe-modal.component";
 //import {components} from './components'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ConsultarDenunciaModalComponent],
+  imports: [RouterOutlet, ConsultarDenunciaModalComponent, ConsultarInformeModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
