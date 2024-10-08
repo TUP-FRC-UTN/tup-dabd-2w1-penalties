cd2670c13bad426f2f2f4a3831c5093bb3bb15b6
113914-MolinaTorres-Julian <113914@tecnicatura.frc.utn.edu.ar>
2024-10-08 19:20:11 -0300

    add complaint interface

---
+++ src/app/models/complaint.ts
@@ -0,0 +1,25 @@
+export interface Complaint {
+    id: number;
+    nombre: string
+    detalle: string
+    tipo: TipoDenuncia;
+    estado: EstadoDenuncia;
+    fecha: Date;
+}
+
+//(aprobada, ignorada, rechazada, pendiente)
+//(incimplimiento daño  infrwaccion de volumen, no manteni miento de limpieza)
+
+export enum EstadoDenuncia {
+    Aprobada = 'aprobada',
+    Ignorada = 'ignorada',
+    Rechazada = 'rechazada',
+    Pendiente = 'pendiente'
+}
+
+export enum TipoDenuncia {
+    IncumplimientoDeVelocidad = 'incumplimiento de velocidad',
+    Daño = 'daño',
+    InfracciónDeinquilino = 'infracción contra el inquilino',
+    NoMantenimientoDeLimpieza = 'no mantenimiento de limpieza'
+}
