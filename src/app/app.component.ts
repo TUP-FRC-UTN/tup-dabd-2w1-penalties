8e04a7a30c0eb696d05832470a0948a7e9e78888
113914-MolinaTorres-Julian <113914@tecnicatura.frc.utn.edu.ar>
2024-10-08 21:12:41 -0300

    add bootstrap icons and httpClient
    Co-Authored-By: 113857DeMaussionGabriel <164547690+113857DeMaussionGabriel@users.noreply.github.com>
    Co-Authored-By: 114264 - Moszoro Mateo <142827268+114264MoszoroMateo@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConsultarDenunciaModalComponent } from './components/consultar-denuncia-modal/consultar-denuncia-modal.component';
 import { ConsultarInformeModalComponent } from "./components/consultar-informe-modal/consultar-informe-modal.component";
+import { GetAllComplaintsComponent } from "./components/get-all-complaints/get-all-complaints.component";
+import { PostComplaintComponent } from "./components/post-complaint/post-complaint.component";
 //import {components} from './components'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConsultarDenunciaModalComponent, ConsultarInformeModalComponent],
+  imports: [RouterOutlet, ConsultarDenunciaModalComponent, ConsultarInformeModalComponent, GetAllComplaintsComponent, PostComplaintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
