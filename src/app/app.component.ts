c936188c982b2e2fa4e287c9385aced9e129ab12
110899SarateLuciana <lusarate@gmail.com>
2024-10-09 20:37:43 -0300

    actualizacion Co-authored-by: 405708 <405708@tecnicatura.frc.utn.edu.ar> Co-authored-by: 114264MoszoroMateo <114264@tecnicatura.frc.utn.edu.ar> Co-authored-by: 113914-MolinaTorres-Julian <113914@tecnicatura.frc.utn.edu.ar>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ConsultarDenunciaModalComponent } from './components/consultar-denuncia-modal/consultar-denuncia-modal.component';
-import { ConsultarInformeModalComponent } from "./components/consultar-informe-modal/consultar-informe-modal.component";
-import { GetAllComplaintsComponent } from "./components/get-all-complaints/get-all-complaints.component";
-import { PostComplaintComponent } from "./components/post-complaint/post-complaint.component";
-import { ListComplaintsComponent } from "./components/list-complaints/list-complaints.component";
+import { ConsultarDenunciaModalComponent } from './components/Complaints/consultar-denuncia-modal/consultar-denuncia-modal.component';
+import { ConsultarInformeModalComponent } from "./components/Penaltis/consultar-informe-modal/consultar-informe-modal.component";
+
+import { PostComplaintComponent } from "./components/Complaints/post-complaint/post-complaint.component";
+import { ListComplaintsComponent } from "./components/Complaints/list-complaints/list-complaints.component";
+
 //import {components} from './components'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConsultarDenunciaModalComponent, ConsultarInformeModalComponent, GetAllComplaintsComponent, PostComplaintComponent, ListComplaintsComponent],
+  imports: [RouterOutlet, ConsultarDenunciaModalComponent, ConsultarInformeModalComponent, PostComplaintComponent, ListComplaintsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
