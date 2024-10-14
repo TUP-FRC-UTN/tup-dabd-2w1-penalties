1f2722f7f9c373e84acbca4d23305fd423f57f97
lusarate <lusarate@gmail.com>
2024-10-13 21:07:58 -0300

    actualizacion
    Co-authored-by: 405708 <405708@tecnicatura.frc.utn.edu.ar>

--- src/app/components/list-complaints/list-complaints.component.ts
+++ src/app/components/list-complaints/list-complaints.component.ts
@@ -10,6 +10,9 @@ import { ModalStateReasonComponent } from '../modal-state-reason/modal-state-rea
 
 import { FormsModule } from '@angular/forms';
 
+import $ from 'jquery';
+import 'datatables.net'
+
 @Component({
   selector: 'app-list-complaints',
   standalone: true,
@@ -32,57 +35,127 @@ constructor(private router: Router,private _modal:NgbModal, private complServ: C
 
 }
   ngOnInit(): void {
-    this.refreshData()
+   
+    this.makeFunctionsGlobal();
+    this.refreshData();
   }
+
   applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+    const filterValue = (event.target as HTMLInputElement).value.toLowerCase().trim(); // Añadido trim()
   
     if (filterValue) {
       this.filterComplaint = this.Complaint.filter((p) => {
-        const complaintStateStr = p.complaintState.toLowerCase();
-        const descriptionStr = p.description.toLowerCase();
+        // Asegúrate que formatDate retorne una fecha en el formato DD/MM/YYYY
+        let fecha = this.formatDate(p.createdDate);
+        
+        const complaintStateStr = p.complaintState.toLowerCase().trim(); // Añadido trim()
+        const descriptionStr = p.description.toLowerCase().trim(); // Añadido trim()
+        // No necesitas convertir fecha de nuevo a Date
+        const fechaStr = fecha ? fecha : ''; // Aquí asumimos que formatDate ya devuelve DD/MM/YYYY
+        
+        console.log(p.createdDate); // Para depuración
   
         return (
-          complaintStateStr.indexOf(filterValue) >= 0 || 
-          descriptionStr.indexOf(filterValue) >= 0
+          complaintStateStr.includes(filterValue) || // Usando includes
+          descriptionStr.includes(filterValue) ||    // Usando includes
+          fechaStr.includes(filterValue)              // Comparando directamente
         );
       });
     } else {
-      
       this.filterComplaint = [...this.Complaint];
     }
-  }
-
-
   
-  viewComplaint(i:number){
-    const modal = this._modal.open(ConsultarDenunciaModalComponent, { size: 'xl' ,  keyboard: false });
-      modal.componentInstance.denunciaId =i ; 
-      modal.result.then((result) => {
-      
-      }).catch((error) => {
-        console.log('Modal dismissed with error:', error);
-      });
+    this.updateDataTable();
   }
   
+  
+
   refreshData() {
-    this.complServ.getAllComplains().subscribe(data=>{
-      this.Complaint=data
+    this.complServ.getAllComplains().subscribe(data => {
+      this.Complaint = data;
       this.filterComplaint = [...data];
       this.collectionSize = data.length; 
-      this.filterComplaint= data.slice((this.page - 1) * this.pageSize, this.page * this.pageSize);
-      this.Complaint.forEach((c) => {
-       
-        if (Array.isArray(c.createdDate)) {
+      this.updateDataTable();
+    });
+  }
+  makeFunctionsGlobal() {
+    (window as any).viewComplaint = (id: number) => this.viewComplaint(id);
+    (window as any).selectState = (state: string, id: number, userId: number) => this.selectState(state, id, userId);
+  }
+  
+  updateDataTable() {
+    // Reinicia la DataTable si ya existe
+    if ($.fn.dataTable.isDataTable('#complaintsTable')) {
+      $('#complaintsTable').DataTable().clear().destroy();
+    }
 
-          console.log()
-          const [year, month, day, hour, minute, second] = c.createdDate;
-          c.createdDate = new Date(year, month - 1, day, hour, minute, second);
+    // Inicializa la DataTable
+    let table= $('#complaintsTable').DataTable({
+      data: this.filterComplaint,
+      columns: [
+        { data: 'createdDate', render: (data) => this.formatDate(data) }, // Formatear fecha
+        {
+          data: 'complaintState',
+          render: (data) => `<button class="btn ${this.getStatusClass(data)} text-center border rounded-pill text-dark" style="width: 130px;">${data}</button>`
+        },
+        { data: 'description' },
+        { data: 'fileQuantity', render: (data) => `<i class="bi bi-paperclip"></i> ${data} Archivo adjunto` },
+        {
+          data: null,
+          render: (data) => `
+             <div class="btn-group gap-2">
+              <button type="button" class="btn btn-success" onclick="viewComplaint(${data.id})" style="border: 3px solid #1B4332;
+                  background-color: #2D6A4F;
+                  color: white;
+                
+                  width: 100px;
+                  height: 50px;
 
+                  border-radius: 15px;
+                  display: flex; 
+                  justify-content: center; 
+                  align-items: center;
+                  text-align: center;
+                  padding: 0;">Ver más</button>
+              <div class="dropdown">
+                <button type="button" class="btn btn-success" data-bs-toggle="dropdown" aria-expanded="false" style=" border: 3px solid #1B4332;
+                  background-color: #2D6A4F;
+                  color: white;
+                  
+                  width: 50px;
+                  height: 50px;
+
+                  border-radius: 15px;
+                  display: flex; 
+                  justify-content: center; 
+                  align-items: center;
+                  text-align: center;
+                  padding: 0;">•••</button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" onclick="selectState('ATTACHED', ${data.id}, ${data.userId})">Marcar como Anexada</a></li>
+                  <li><a class="dropdown-item" onclick="selectState('REJECTED', ${data.id}, ${data.userId})">Marcar como Rechazada</a></li>
+                  <li><a class="dropdown-item" onclick="selectState('PENDING', ${data.id}, ${data.userId})">Marcar como Pendiente</a></li>
+                </ul>
+              </div>
+            </div>`
         }
-      });
-     
-    })
+      ],
+      paging: true,
+      pageLength: this.pageSize,
+      searching: false // Deshabilitar la búsqueda global predeterminada
+      //,dom: '<"top"i>rt<"bottom"flp><"clear">',
+      
+    });
+    
+  }
+
+  formatDate(date: any): string {
+    if (Array.isArray(date)) {
+      const [year, month, day, hour, minute, second] = date;
+      const createdDate = new Date(year, month - 1, day, hour, minute, second);
+      return createdDate.toLocaleDateString('es-ES'); 
+    }
+    return new Date(date).toLocaleDateString('es-ES'); 
   }
 
   // Metodo para obtener el estado de la denuncia y mostrar el modal 
@@ -103,14 +176,23 @@ constructor(private router: Router,private _modal:NgbModal, private complServ: C
       console.log('Modal dismissed with error:', error);
     });
   }
-
+  
+  viewComplaint(i:number){
+    const modal = this._modal.open(ConsultarDenunciaModalComponent, { size: 'xl' ,  keyboard: false });
+      modal.componentInstance.denunciaId =i ; 
+      modal.result.then((result) => {
+      
+      }).catch((error) => {
+        console.log('Modal dismissed with error:', error);
+      });
+  }
 
 
   getStatusClass(estado: string): string {
     switch (estado) {
       case 'Anexada':
         return 'btn-secondary';
-      case 'Nuevo':
+      case 'Nueva':
         return 'btn-success';
       case 'Pendiente':
         return 'btn-warning';
