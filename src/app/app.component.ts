9a2417735f159016719ab186ffdf702683ad3f38
lusarate <lusarate@gmail.com>
2024-10-16 19:07:01 -0300

    Reorganizacion Co-authored-by: 405708 <405708@tecnicatura.frc.utn.edu.ar>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ConsultarDenunciaModalComponent } from './components/modal-complaint/consultar-denuncia-modal.component';
-import { ConsultarInformeModalComponent } from "./components/modal-report/consultar-informe-modal.component";
-import { GetAllComplaintsComponent } from "./components/get-all-complaints/get-all-complaints.component";
-import { PostComplaintComponent } from "./components/post-complaint/post-complaint.component";
-import { ListComplaintsComponent } from "./components/list-complaints/list-complaints.component";
 //import {components} from './components'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConsultarDenunciaModalComponent, ConsultarInformeModalComponent, GetAllComplaintsComponent, PostComplaintComponent, ListComplaintsComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-import { ListComplaintsComponent } from './components/list-complaints/list-complaints.component';
-import { ConsultarDenunciaModalComponent } from './components/modal-complaint/consultar-denuncia-modal.component';
-import { PostComplaintComponent } from './components/post-complaint/post-complaint.component';
+import { PenaltiesPostComplaintComponent } from './components/complaintComponents/penalties-post-complaint/penalties-post-complaint.component';
+import { PenaltiesListComplaintComponent } from './components/complaintComponents/penalties-list-complaint/penalties-list-complaint.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/list', pathMatch: 'full' },  
-    { path: 'list', component: ListComplaintsComponent },  
-    { path: 'postComplaint', component: PostComplaintComponent },  
+    { path: 'list', component: PenaltiesListComplaintComponent },  
+    { path: 'postComplaint', component: PenaltiesPostComplaintComponent },  
 ];
