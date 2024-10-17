ff726efe0a3f35c9c7690403b1f48d8ff8af6e2e
114264 - Moszoro Mateo <114264@tecnicatura.frc.utn.edu.ar>
2024-10-17 20:10:39 -0300

    post-fine-component
    Co-Authored-By: Juan Pablo Bauza <142741295+404994-Bauza-Juan-Pablo@users.noreply.github.com>

---
+++ src/app/components/sanctionsComponents/penalties-post-fine/penalties-post-fine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-penalties-post-fine',
+  templateUrl: './penalties-post-fine.component.html',
+  styleUrls: ['./penalties-post-fine.component.css']
+})
+export class PenaltiesPostFineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
